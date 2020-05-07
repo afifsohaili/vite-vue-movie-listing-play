9c7665b7424b9a2ce7777c2737e557ac0de7fd07
Afif Sohaili <afifnajib@gmail.com>
2020-05-07 15:06:29 +0800

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>
+    App
+  </h1>
+  <v-button @click="count++">Count: {{count}}</v-button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import VButton from './components/VButton.vue'
+
+export default defineComponent({
+  components: {
+    VButton,
+  },
+
+  setup() {
+    const count = ref(0)
+
+    return {
+      count,
+    }
+  },
+})
+</script>
