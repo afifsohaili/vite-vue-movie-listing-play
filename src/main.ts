0198fa0f2ae48a5997589d443b99fe1a9652d929
Afif Sohaili <afifnajib@gmail.com>
2020-11-14 09:48:00 +0800

    Add icomoon font for icons.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import './main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'normalize.css/normalize.css'
-import './index.css'
+import './assets/index.css'
+import './assets/icons.css'
 
 createApp(App).mount('#app')
